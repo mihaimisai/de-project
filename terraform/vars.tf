baaa714d28badafdbd7b9643ff32d3bbd0d6103f
A F <af@Ahmads-MacBook-Air.local>
2025-02-24 16:44:15 +0000

    vars

---
+++ terraform/vars.tf
@@ -0,0 +1,35 @@
+#check when bucket is created
+variable "state_bucket" { 
+    type = string
+    default = "de-project-terraform-state"
+}
+variable "data_bucket_prefix" {
+    type = string
+    default = "de-project-ingested-data-"
+}
+variable "code_bucket_prefix" {
+    type = string
+    default = "de-project-lambda-code-"
+}
+variable "lambda_1_name" {
+    type = string
+    default = "data_ingester_lambda"
+}
+
+variable "lambda_2_name" {
+    type = string
+    default = "data_processer_lambda"
+}
+variable "lambda_3_name" {
+    type = string
+    default = "data_storer_lambda"
+}
+variable "python_runtime" {
+    type = string
+    default = "python3.12"
+}
+variable "alert_email" {
+    type = string
+    default = ""# add email
+
+}
