3e61d922a9a1f2edba46b3d011e1c538c6451b09
mesuhe <meralhewitt@gmail.com>
2025-02-25 17:47:08 +0000

    comment out secret vars so doesn't hang on terraform plan as not needed until actually testing lambda python code - add back in when secrets added (via AWS SM or github secrets)

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,30 +1,30 @@
-variable "db_user" { 
-  type = string 
-  }
+# variable "db_user" { 
+#   type = string 
+#   }
 
-variable "db_password" { 
-  type = string 
-  }
+# variable "db_password" { 
+#   type = string 
+#   }
 
-variable "db_host" { 
-  type = string 
-  }
+# variable "db_host" { 
+#   type = string 
+#   }
 
-variable "db_db" { 
-  type = string 
-  }
+# variable "db_db" { 
+#   type = string 
+#   }
 
-variable "db_port" { 
-  type = number 
-  }
+# variable "db_port" { 
+#   type = number 
+#   }
 
-variable "log_email" {
-  type = string
-}
+# variable "log_email" {
+#   type = string
+# }
 
-variable "log_email_pass" {
-  type = string
-}
+# variable "log_email_pass" {
+#   type = string
+# }
 
 
 # variable "state_bucket" { 
