d7f06ccb99fac65344cc03535a6f2061eaaa89ea
Anna Fedyna <annafedyna03@gmail.com>
2025-02-25 12:42:07 +0000

    added code for lambda inside the bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -21,3 +21,12 @@ resource "aws_s3_bucket" "code_bucket" {
     Environment = "Dev"
   }
 }
+
+#TODO: Upload the ingester_lambda code to the code_bucket
+resource "aws_s3_object" "ingester_lambda_code" {
+  bucket = aws_s3_bucket.code_bucket.bucket
+  key = "lambda/ingester_lambda.zip"
+  source = "${path.module}/../ingester_function.zip" 
+}
+
+#TODO: Upload the ingester_lambda layer to the code_bucket if exists
