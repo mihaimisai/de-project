61f99fb726e61e0fff2d1b232b5ffd299bff2db8
Mihai <mihaimisai@gmail.com>
2025-02-25 11:53:10 +0000

    created variables for db and email credentials, created secrets manager and added in the variables, updated the yml file to pass in these credentials as secrets

---
+++ terraform/secrets-manager.tf
@@ -0,0 +1,27 @@
+resource "aws_secretsmanager_secret" "db_secret" {
+  name = "db_credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "db_secret_version" {
+  secret_id     = aws_secretsmanager_secret.db_secret.id
+  secret_string = jsonencode({
+    db_user    = var.db_user
+    db_password = var.db_password
+    db_host    = var.db_host
+    db_db      = var.db_db
+    db_port    = var.db_port
+  })
+  
+}
+
+resource "aws_secretsmanager_secret" "email_secret" {
+  name = "log_email_credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "email_secret_version" {
+  secret_id     = aws_secretsmanager_secret.email_secret.id
+  secret_string = jsonencode({
+    log_email    = var.log_email
+    log_email_pass = var.log_email_pass
+  })
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,27 @@
+variable "db_user" { 
+  type = string 
+  }
+
+variable "db_password" { 
+  type = string 
+  }
+
+variable "db_host" { 
+  type = string 
+  }
+
+variable "db_db" { 
+  type = string 
+  }
+
+variable "db_port" { 
+  type = number 
+  }
+
+variable "log_email" {
+  type = string
+}
+
+variable "log_email_pass" {
+  type = string
+}
