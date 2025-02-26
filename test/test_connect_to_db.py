5997f49096a29e9a14590e9a14234dfe749141df
Shea Macfarlane <shea@Sheas-MacBook-Pro.local>
2025-02-26 15:40:17 +0000

    Logging fully tested

--- test/test_connect_to_db.py
+++ test/test_connect_to_db.py
@@ -5,10 +5,15 @@ import unittest
 from unittest.mock import patch, Mock
 import pg8000 
 from pg8000.exceptions import InterfaceError, DatabaseError
+from testfixtures import LogCapture
 
 @pytest.fixture
 def test_logger():
-    return logging.getLogger("test_logger")
+    logger = logging.getLogger("test_logger")
+    logger.setLevel(logging.INFO)
+    return logger
+
+
 
 class TestConnectToDb:
     @patch("src.utils.connect_to_db.pg_access", return_value=("localhost", 1234, "test_db", "user", "password"))
@@ -29,16 +34,27 @@ class TestConnectToDb:
             password="password"
         )
     
-
     # @patch("src.utils.connect_to_db.pg_access", return_value=("incorrect"))
-    # def test_connects_to_db_fails_with_no_connection_established_log(self, caplog):
-    #     with caplog.at_level(logging.DEBUG):
-    #         try:
-    #             connect_to_db(test_logger)
-    #         except Exception:
-    #             pass
+    def test_connects_to_db_logs_error(self, test_logger):
+        with LogCapture() as l:
+            try:
+                connect_to_db(test_logger)
+            except:
+                pass
+        for x in l:
+            assert x == ('test_logger', 'ERROR', 'Connection failed: One or more PostgreSQL credentials are missing.')
+    
+    @patch("src.utils.connect_to_db.pg_access", return_value=("localhost", 1234, "test_db", "user", "password"))
+    @patch("src.utils.connect_to_db.Connection", return_value=object())
+    def test_connects_to_db_logs_info(self, mock_access, mock_connection, test_logger):
+        with LogCapture() as l:
+            try:
+                connect_to_db(test_logger)
+            except:
+                pass
         
-    #     print(caplog)
-    #     assert "Connection failed" in caplog.text
-        
+        for x in l:
+            assert x == ('test_logger', 'INFO', 'Connecting to PostgreSQL database: test_db on host: localhost')
+    
+    
 
