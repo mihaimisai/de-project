8bbd95a4bc15c8ef29b76fe286fb1eefa3297d8d
mesuhe <meralhewitt@gmail.com>
2025-02-26 10:30:09 +0000

    add sensitive = true to variables accessed through secrets

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,30 +1,42 @@
-# variable "db_user" { 
-#   type = string 
-#   }
+variable "db_user" { 
+  type = string 
+  sensitive = true
+  }
 
-# variable "db_password" { 
-#   type = string 
-#   }
+variable "db_password" { 
+  type = string 
+  sensitive = true
+  }
 
-# variable "db_host" { 
-#   type = string 
-#   }
+variable "db_host" { 
+  type = string 
+  sensitive = true
+  }
 
-# variable "db_db" { 
-#   type = string 
-#   }
+variable "db_db" { 
+  type = string 
+  sensitive = true
+  }
 
-# variable "db_port" { 
-#   type = number 
-#   }
+variable "db_port" { 
+  type = number 
+  sensitive = true
+  }
 
 # variable "log_email" {
 #   type = string
 # }
 
-# variable "log_email_pass" {
-#   type = string
-# }
+variable "alert_email" {
+    type = string
+    sensitive = true
+    # default = "NotificationStreamBanshee@Gmail.com"   
+}
+
+variable "log_email_pass" {
+  type = string
+  sensitive = true
+}
 
 
 # variable "state_bucket" { 
@@ -35,31 +47,33 @@ variable "ingested_data_bucket_prefix" {
     type = string
     default = "de-project-ingested-data-"
 }
+
 variable "code_bucket_prefix" {
     type = string
     default = "de-project-lambda-code-"
 }
+
 variable "timestamp_bucket_prefix" {
   type = string
     default = "de-project-lambda-timestamp-"
 }
+
 variable "lambda_1_name" {
     type = string
     default = "data_ingester_lambda"
 }
-variable "lambda_2_name" {
-    type = string
-    default = "data_processer_lambda"
-}
-variable "lambda_3_name" {
-    type = string
-    default = "data_storer_lambda"
-}
+
+# variable "lambda_2_name" {
+#     type = string
+#     default = "data_processer_lambda"
+# }
+
+# variable "lambda_3_name" {
+#     type = string
+#     default = "data_storer_lambda"
+# }
+
 variable "python_runtime" {
     type = string
     default = "python3.12"
-}
-variable "alert_email" {
-    type = string
-    default = "NotificationStreamBanshee@Gmail.com"
 }
