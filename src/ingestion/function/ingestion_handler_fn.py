4878f8a6a7b58dd540b223149b1e8dd463b22f01
mesuhe <meralhewitt@gmail.com>
2025-02-27 23:40:29 +0000

    force update to lambda

--- src/ingestion/function/ingestion_handler_fn.py
+++ src/ingestion/function/ingestion_handler_fn.py
@@ -2,9 +2,9 @@ from .utils.s3_client import s3_client
 from .utils.process_all_tables import process_all_tables
 
 
-def ingestion_handler(event, context):
+# def ingestion_handler(event, context):
 
-    client = s3_client()
-    process_all_tables(client)
+#     client = s3_client()
+#     process_all_tables(client)
 
-    return {"statusCode": 200, "body": "Data ingestion complete"}
+#     return {"statusCode": 200, "body": "Data ingestion complete"}
