0d0d50b0e96a728201689c652e20a64343e8a61e
mesuhe <meralhewitt@gmail.com>
2025-02-27 17:33:15 +0000

    change references in lambda_handler and test

--- src/ingestion/ingestion_handler.py
+++ src/ingestion/ingestion_handler.py
@@ -1,5 +1,5 @@
-from src.utils.s3_client import s3_client
-from src.utils.process_all_tables import process_all_tables
+from src.ingestion.utils.s3_client import s3_client
+from src.ingestion.utils.process_all_tables import process_all_tables
 
 
 def ingestion_handler(event, context):
