fac184f2851aff2f7cf4d143b062a9dc324cd485
mesuhe <meralhewitt@gmail.com>
2025-02-27 23:48:29 +0000

    change permission list bucket

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -52,7 +52,7 @@ data "aws_iam_policy_document" "s3_data_policy_doc" {
 
     actions = ["s3:ListBucket"]
     resources = [
-      "${aws_s3_bucket.timestamp_bucket.arn}/*"
+      "${aws_s3_bucket.timestamp_bucket.arn}"
     ]
     effect = "Allow"
   }
