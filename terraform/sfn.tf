94ec56792142beab9b42cc7158197792d07d0e88
Shea Macfarlane <shea@Sheas-MacBook-Pro.local>
2025-03-03 16:35:52 +0000

    scheduler linked to state-machine

--- terraform/sfn.tf
+++ terraform/sfn.tf
@@ -1,6 +1,6 @@
 
 resource "aws_sfn_state_machine" "sfn_state_machine_ingest_to_transform" {
-  name_prefix = var.state_machine_name
+  name = var.state_machine_name
   definition = templatefile("${path.module}/state-machine-definition.json",
     { aws_region        = "${data.aws_region.current.name}",
       aws_account_num   = "${data.aws_caller_identity.current.account_id}",
