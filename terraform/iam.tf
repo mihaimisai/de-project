83777f2e7634ffebbb78ef5ec57b3ae05a3102cb
Shea Macfarlane <shea@Sheas-MacBook-Pro.local>
2025-03-04 10:30:26 +0000

    Added principal to IAM policy.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -9,7 +9,7 @@ data "aws_iam_policy_document" "trust_policy" {
 
     principals {
       type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
+      identifiers = ["lambda.amazonaws.com", "states.amazonaws.com"]
     }
 
     actions = ["sts:AssumeRole"]
