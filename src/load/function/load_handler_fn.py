ce16f8db9632b58ecd56e2befc69947f8c1d761a
Anna Fedyna <annafedyna03@gmail.com>
2025-03-05 16:39:05 +0000

    feac: main load handler logic

--- src/load/function/load_handler_fn.py
+++ src/load/function/load_handler_fn.py
@@ -1,4 +1,10 @@
 from .utils.s3_client import s3_client
+from utils.get_parquet_from_s3 import get_parquet_from_s3
+from utils.create_table_in_db import create_table_in_db
+from utils.insert_dataframe_in_db import insert_dataframe_in_db
+from utils.connect_to_dw import connect_to_db
+import logging
+import os
 
 
 def load_handler(event, context):
@@ -6,17 +12,34 @@ def load_handler(event, context):
     Orchestrates load and storage of data
     from processed bucket into warehouse.
 
-    Invokes s3_client and process_all_tables.
+    Parameters:
+        event (event object): json object - converted to empty dict
+        by lambda python runtime
+        context (context object): context object provided by AWS
 
-        Parameters:
-            event (event object): json object - converted to empty dict
-            by lambda python runtime
-            context (context object): context object provided by AWS
-
-        Returns:
-            dictionary, {'statusCode': 200, "body": "Data ingestion complete" }
-            if successful
+    Returns:
+        dictionary, {'statusCode': 200, "body": "Data ingestion complete" }
+        if successful
     """
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
     client = s3_client()
+    load_bucket_name = os.environ.get("processed_data_bucket")
+    try:
+        conn = connect_to_db(logger)
+        df_data_tables = get_parquet_from_s3(logger, client, load_bucket_name)
 
-    return {"statusCode": 200, "body": "Data ingestion complete"}
+        for table_name, df in df_data_tables.items():
+            table_columns = ", ".join(df.columns)
+            create_table_in_db(conn, table_name, table_columns, logger)
+            insert_dataframe_in_db(conn, table_name, df, logger)
+
+        return {"statusCode": 200, "body": "Data ingestion complete"}
+    except Exception as e:
+        logger.error(f"Error in load_handler: {e}")
+        return {"statusCode": 500, "body": f"Error: {e}"}
+    finally:
+        if "conn" in locals() and conn:
+            conn.close()
