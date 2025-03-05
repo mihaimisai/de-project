f1b32e48c94af9a9d3d3c443049b2fe609e29d2e
Shea Macfarlane <shea@Sheas-MacBook-Pro.local>
2025-03-05 11:13:50 +0000

    Load handler shea branch

---
+++ src/load/function/utils/s3_client.py
@@ -0,0 +1,14 @@
+import boto3
+def s3_client(REGION="eu-west-2"):
+    """
+    Initializes s3 client.
+        Parameters:
+            region (str): AWS region (default is 'eu-west-2')
+        Returns:
+            s3 client
+    """
+    try:
+        client = boto3.client("s3", region_name=REGION)
+        return client
+    except Exception as e:
+        raise e
