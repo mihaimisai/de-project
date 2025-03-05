ebc6149e545195ce6ab0c441670481eae4308190
Shea Macfarlane <shea@Sheas-MacBook-Pro.local>
2025-03-05 16:24:57 +0000

    DW credentials access and load for lambda 3.

---
+++ src/load/function/utils/load_credentials_for_data_warehouse.py
@@ -0,0 +1,27 @@
+import os
+
+
+def dw_access():
+    """
+    Retrieves database Data-Warehouse credentials
+    from lambda environment variables.
+
+        Returns:
+            list of database credential variables
+            [PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD]
+
+        Raises:
+            ValueError("One or more PostgreSQL credentials
+            are missing.") if unsuccessful
+    """
+    # Retrieve credentials from environment variables
+    PG_HOST = os.environ.get("DB_HOST_DW")
+    PG_PORT = os.environ.get("DB_PORT_DW")
+    PG_DATABASE = os.environ.get("DB_DW")
+    PG_USER = os.environ.get("DB_USER_DW")
+    PG_PASSWORD = os.environ.get("DB_PASSWORD_DW")
+
+    if not all([PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD]):
+        raise ValueError("One or more PostgreSQL credentials for the Data Warehouse are missing.") # noqa
+
+    return [PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD]
